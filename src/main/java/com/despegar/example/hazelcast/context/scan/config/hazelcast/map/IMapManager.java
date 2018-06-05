57f7a6e45a63470291dad7debb2b8951ff6d9858
Leandro Carrasco <lcarrasco@despegar.com>
2018-06-05 15:40:17 -0300

    #UPDATE

--- src/main/java/com/despegar/example/hazelcast/context/scan/config/hazelcast/map/IMapManager.java
+++
@@ -1,8 +0,0 @@
-package com.despegar.example.hazelcast.context.scan.config.hazelcast.map;
-
-import com.hazelcast.core.IMap;
-
-public interface IMapManager<K, V> {
-
-    IMap<K, V> getMap();
-}
--- src/main/java/com/despegar/example/hazelcast/repository/model/generic/HibernateEntity.java
+++
@@ -1,8 +0,0 @@
-package com.despegar.example.hazelcast.repository.model.generic;
-
-public interface HibernateEntity<ID> {
-
-    ID getId();
-
-    void setId(ID id);
-}
